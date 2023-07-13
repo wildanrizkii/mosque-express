52589e8710138ea0c97431e94ac21b695ee0b45e
Wildan Rizki Nurfauzi <63538803+wildanrizkii@users.noreply.github.com>
2023-07-13 21:28:03 +0700

    mengganti api

--- index.js
+++ index.js
@@ -25,7 +25,10 @@ app.get("/", async (req, res) => {
     // const APIKegiatan = 'https://v1.nocodeapi.com/wildanrizkii/google_sheets/StuMBMJbZAHAfgVd?tabId=Kegiatan'
 
     // SheetDB API
-    const APIKegiatan = 'https://sheetdb.io/api/v1/cwocbdhpkx4z4'
+    // const APIKegiatan = 'https://sheetdb.io/api/v1/cwocbdhpkx4z4'
+
+    // gsx2json API
+    const APIKegiatan = 'https://gsx2json.com/api?id=1OFsj9Bm84BF1qGzEWhLZ350D099XiB2tsRChD7KuzpE&sheet=Sheet1&api_key=AIzaSyD17_QyNnk3j3Wbf8yxUugz4VVLsAJtqXM'
 
     let waktuSekarang, tahun, bulan, tanggal, timeSubscribe, time, jadwalAPI, jadwal, jadwalSholat;
 
@@ -131,110 +134,19 @@ app.get("/", async (req, res) => {
     let waktuIsya = await getSholatIsya(jadwalSholat);
 
     // SheetDB
-    let dataKegiatan = await getKegiatan(APIKegiatan);
+    // let dataKegiatan = await getKegiatan(APIKegiatan);
 
     // NoCodeAPI
     // let kegiatan = await getKegiatan(APIKegiatan);
     // let dataKegiatan = kegiatan.data
 
+    // gsx2json
+    let kegiatan = await getKegiatan(APIKegiatan);
+    let dataKegiatan = kegiatan.rows
+
     res.status(200).render('home', { 
         jadwalSholat, waktuSekarang, waktuSubuh, waktuDzuhur, waktuAshar, waktuMaghrib, waktuIsya, timeSubscribe, dataKegiatan 
     });
-
-    // fetch(timeAPI, {
-    //     method: 'GET',
-    //     headers: {
-    //       'X-Api-Key': APIKey
-    //     },
-    //     contentType: 'application/json',
-    //     json: true
-    // })
-    // .then(function(output) {
-    //     return output.json();
-    // })
-    // .then(function (time) {
-    //     waktuSekarang = parseInt(time.hour * 60) + parseInt(time.minute)
-    //     tahun = time.year
-    //     bulan = time.month
-    //     tanggal = time.day
-    //     timeSubscribe = time.datetime
-    //     let URL = 'https://api.myquran.com/v1/sholat/jadwal/1219/' + tahun + '/' + bulan + '/' + tanggal
-
-    //     fetch(URL)
-    //         .then(function (response) {
-    //             return response.json();
-    //         })
-    //         .then(function (data) {
-    //             let jadwalSholat = data.data.jadwal
-
-    //             // Subuh
-    //             let subuh = jadwalSholat.subuh
-    //             let waktuJamSubuh = subuh.split(':')[0] * 60
-    //             let waktuMenitSubuh = subuh.split(':')[1]
-    //             let waktuSubuh = parseInt(waktuJamSubuh) + parseInt(waktuMenitSubuh)
-
-    //             // Dzuhur
-    //             let dzuhur = jadwalSholat.dzuhur
-    //             let waktuJamDzuhur = dzuhur.split(':')[0] * 60
-    //             let waktuMenitDzuhur = dzuhur.split(':')[1]
-    //             let waktuDzuhur = parseInt(waktuJamDzuhur) + parseInt(waktuMenitDzuhur)
-
-    //             // Ashar
-    //             let ashar = jadwalSholat.ashar
-    //             let waktuJamAshar = ashar.split(':')[0] * 60
-    //             let waktuMenitAshar = ashar.split(':')[1]
-    //             let waktuAshar = parseInt(waktuJamAshar) + parseInt(waktuMenitAshar)
-
-    //             // Maghrib
-    //             let maghrib = jadwalSholat.maghrib
-    //             let waktuJamMaghrib = maghrib.split(':')[0] * 60
-    //             let waktuMenitMaghrib = maghrib.split(':')[1]
-    //             let waktuMaghrib = parseInt(waktuJamMaghrib) + parseInt(waktuMenitMaghrib)
-
-    //             // Isya
-    //             let isya = jadwalSholat.isya
-    //             let waktuJamIsya = isya.split(':')[0] * 60
-    //             let waktuMenitIsya = isya.split(':')[1]
-    //             let waktuIsya = parseInt(waktuJamIsya) + parseInt(waktuMenitIsya)
-
-    //             // const readFileAPI = fs.readFileSync(path.join(staticPath, '../api/kegiatan_api.json'),
-    //             //     { encoding: 'utf-8' });
-    //             // const jsonData = JSON.parse(readFileAPI);
-    //             // const dataKegiatan = jsonData.kegiatan
-
-    //             fetch(APIKegiatan)
-    //                 .then(function (res) {
-    //                     return res.json();
-    //                 })
-    //                 .then(function (data) {
-    //                     // SheetDB : dataKegiatan = data
-    //                     // NoCodeAPI : dataKegiatan = data.data
-    //                     // dataKegiatan = data
-    //                     dataKegiatan = `[
-    //                         {
-    //                             {
-    //                                 no_kegiatan: '5',
-    //                                 nama_kegiatan: 'Pemasangan CCTV',
-    //                                 hari: 'Minggu',
-    //                                 day: '16',
-    //                                 month: '7',
-    //                                 bulan: 'Juli',
-    //                                 year: '23',
-    //                                 tahun: '2023',
-    //                                 start_time: '14:00',
-    //                                 end_time: 'selesai',
-    //                                 deskripsi: 'Bebersih Masjid',
-    //                                 ruangan: 'Masjid Al-Hikmah',
-    //                                 link_meeting: '-',
-    //                                 pemateri: 'Pemuda Masjid'
-    //                             }
-    //                         }
-    //                     ]`
-    //                     res.status(200).render('home', { jadwalSholat, waktuSekarang, waktuSubuh, waktuDzuhur, waktuAshar, waktuMaghrib, waktuIsya, timeSubscribe, dataKegiatan });
-    //                 })
-
-    //         });
-    //     })
 });
 
 app.listen(PORT, () => console.log(`Ready!`));
